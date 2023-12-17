55d4a4835921fd6c046d577a2f91f8b44a8f4249
WardM99 <wardmeersman@gmail.com>
2023-12-17 20:22:55 +0100

    :test_tube: added more logic_messages test

--- backend/tests/test_app/test_logic/test_messages_logic.py
+++ backend/tests/test_app/test_logic/test_messages_logic.py
@@ -1,6 +1,8 @@
 from typing import List
 
+import pytest
 from sqlmodel.ext.asyncio.session import AsyncSession
+from src.app.exceptions.wronguser import WrongUserException
 from src.app.logic.messages_logic import (
     logic_delete_message,
     logic_edit_message,
@@ -59,8 +61,18 @@ async def test_edit_message(database_session: AsyncSession):
     user: User = await make_user(database_session, "Owner", "pw1")
     group: Group = await make_group(database_session, user, "Group1")
     new_message: Message = await logic_make_message(database_session, user, group, "First")
-    updated_message: Message = await logic_edit_message(database_session, new_message, "First!")
+    updated_message: Message = await logic_edit_message(database_session, new_message, "First!", user)
     assert updated_message.message == "First!"
+    
+
+async def test_edit_message_wrong_user(database_session: AsyncSession):
+    user: User = await make_user(database_session, "Owner", "pw1")
+    group: Group = await make_group(database_session, user, "Group1")
+    new_message: Message = await logic_make_message(database_session, user, group, "First")
+    user2: User = await make_user(database_session, "user2", "pw1")
+    with pytest.raises(WrongUserException):
+        await logic_edit_message(database_session, new_message, "First!", user2)
+    assert new_message.message == "First"
 
 
 async def test_delete_message(database_session: AsyncSession):
@@ -70,6 +82,20 @@ async def test_delete_message(database_session: AsyncSession):
     await logic_make_message(database_session, user1, group1, "Second")
     messages: List[Message] = await logic_get_messages_by_group(database_session, group1)
     assert len(messages) == 2
-    await logic_delete_message(database_session, message1)
+    await logic_delete_message(database_session, message1, user1)
     messages = await logic_get_messages_by_group(database_session, group1)
     assert len(messages) == 1
+
+
+async def test_delete_message_wrong_user(database_session: AsyncSession):
+    user1: User = await make_user(database_session, "Owner", "pw1")
+    group1: Group = await make_group(database_session, user1, "Group1")
+    message1: Message = await logic_make_message(database_session, user1, group1, "First")
+    await logic_make_message(database_session, user1, group1, "Second")
+    messages: List[Message] = await logic_get_messages_by_group(database_session, group1)
+    assert len(messages) == 2
+    user2: User = await make_user(database_session, "User2", "pw1")
+    with pytest.raises(WrongUserException):
+        await logic_delete_message(database_session, message1, user2)
+    messages = await logic_get_messages_by_group(database_session, group1)
+    assert len(messages) == 2
