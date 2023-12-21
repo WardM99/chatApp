d39b6697b266629dcfd4352c7943d235ec73b416
Ward Meersman <48222993+WardM99@users.noreply.github.com>
2023-12-21 19:09:37 +0100

    🐛 back populates and schemas (#31)
    * added back_populates to models
    
    * return schemas also return a helper object

--- backend/src/app/logic/messages_logic.py
+++ backend/src/app/logic/messages_logic.py
@@ -5,6 +5,7 @@ from sqlmodel.ext.asyncio.session import AsyncSession
 from src.app.exceptions.wronguser import WrongUserException
 from src.app.logic.groups_logic import logic_get_group_by_id
 from src.app.logic.users_logic import logic_get_user_by_name
+from src.app.schemas.message import ReturnMessage, ReturnMessages
 from src.database.crud.message import (
     get_messages_by_group,
     get_messages_by_user_in_group,
@@ -25,32 +26,62 @@ async def logic_get_message_by_id(
     return await get_message_by_id(database, message_id)
 
 
+async def message_to_return_message(message: Message, database: AsyncSession) -> ReturnMessage:
+    """Helper function to change a Message to a ReturnMessage"""
+    reply_message: Optional[Message] = None
+    if message.reply_id:
+        reply_message = await get_message_by_id(database, message.reply_id)
+    return ReturnMessage(
+        message_id=message.message_id,
+        message=message.message,
+        sender_id=message.sender_id,
+        sender=message.sender,
+        group_id=message.group_id,
+        group=message.group,
+        reply_id=message.reply_id,
+        reply=reply_message
+    )
+
+
 async def logic_make_message(
         database: AsyncSession,
         user: User,
         group: Group,
         message: str,
         reply_id: Optional[int] = None
-) -> Message:
+) -> ReturnMessage:
     """Logic to make a message"""
-    return await make_message(database, user, group, message, reply_id)
+    new_message: Message = await make_message(database, user, group, message, reply_id)
+    return await message_to_return_message(new_message, database)
 
 
 async def logic_get_messages_by_user_in_group(
         database: AsyncSession = Depends(get_session),
         user: User = Depends(logic_get_user_by_name),
         group: Group = Depends(logic_get_group_by_id)
-) -> List[Message]:
+) -> ReturnMessages:
     """Logic to get all messages send by a user in a group"""
-    return await get_messages_by_user_in_group(database, user, group)
-
+    messages_by_user_in_group: List[Message] =\
+        await get_messages_by_user_in_group(database, user, group)
+    return_messages: List[ReturnMessage] = []
+    for m in messages_by_user_in_group:
+        return_messages.append(
+            await message_to_return_message(m, database)
+        )
+    return ReturnMessages(messages=return_messages)
 
 async def logic_get_messages_by_group(
         database: AsyncSession = Depends(get_session),
         group: Group = Depends(logic_get_group_by_id)
-) -> List[Message]:
+) -> ReturnMessages:
     """Logic to get all messages in a group"""
-    return await get_messages_by_group(database, group)
+    messages_in_group: List[Message] = await get_messages_by_group(database, group)
+    return_messages: List[ReturnMessage] = []
+    for m in messages_in_group:
+        return_messages.append(
+            await message_to_return_message(m, database)
+        )
+    return ReturnMessages(messages=return_messages)
 
 
 async def logic_edit_message(
@@ -58,11 +89,13 @@ async def logic_edit_message(
         message: Message,
         new_message: str,
         user: User
-) -> Message:
+) -> ReturnMessage:
     """Logic to edit a message"""
     if message.sender_id != user.user_id:
         raise WrongUserException
-    return await edit_message(database, message, new_message)
+    return await message_to_return_message(
+        await edit_message(database, message, new_message), database
+    )
 
 
 async def logic_delete_message(
--- backend/tests/test_app/test_logic/test_messages_logic.py
+++ backend/tests/test_app/test_logic/test_messages_logic.py
@@ -12,8 +12,10 @@ from src.app.logic.messages_logic import (
     logic_make_message,
     logic_get_message_by_id
 )
+from src.app.schemas.message import ReturnMessage, ReturnMessages
 from src.database.crud.user import make_user
 from src.database.crud.group import make_group
+from src.database.crud.message import make_message
 from src.database.models import User, Group, Message
 
 async def test_logic_make_message(database_session: AsyncSession):
@@ -27,8 +29,9 @@ async def test_logic_get_message_by_id(database_session: AsyncSession):
     user: User = await make_user(database_session, "user1", "pw1")
     group: Group = await make_group(database_session, user, "Group1")
     message: Message = await logic_make_message(database_session, user, group, "message1")
-    message_by_id: Message = await logic_get_message_by_id(message.message_id, database_session)
-    assert message == message_by_id
+    message_by_id: ReturnMessage = await logic_get_message_by_id(message.message_id, database_session)
+    assert message.message_id == message_by_id.message_id
+    assert message.message == message_by_id.message
 
 
 async def test_logic_get_message_by_id_dont_exist(database_session: AsyncSession):
@@ -44,17 +47,17 @@ async def test_logic_get_messages_by_user_in_group(database_session: AsyncSessio
     user2: User = await make_user(database_session, "User2", "pw1")
     group1: Group = await make_group(database_session, user1, "Group1")
     group2: Group = await make_group(database_session, user1, "Group2")
-    message1: Message = await logic_make_message(database_session, user1, group1, "First")
-    message2: Message = await logic_make_message(database_session, user1, group1, "Second")
-    message3: Message = await logic_make_message(database_session, user2, group1, "3th")
-    message4: Message = await logic_make_message(database_session, user1, group2, "4th")
+    message1: ReturnMessage = await logic_make_message(database_session, user1, group1, "First")
+    message2: ReturnMessage = await logic_make_message(database_session, user1, group1, "Second")
+    message3: ReturnMessage = await logic_make_message(database_session, user2, group1, "3th")
+    message4: ReturnMessage = await logic_make_message(database_session, user1, group2, "4th")
 
-    messages: List[Message] = await logic_get_messages_by_user_in_group(database_session, user1, group1)
-    assert len(messages) == 2
-    assert message1 in messages
-    assert message2 in messages
-    assert message3 not in messages
-    assert message4 not in messages
+    messages: ReturnMessages = await logic_get_messages_by_user_in_group(database_session, user1, group1)
+    assert len(messages.messages) == 2
+    assert message1 in messages.messages
+    assert message2 in messages.messages
+    assert message3 not in messages.messages
+    assert message4 not in messages.messages
 
 
 async def test_logic_get_messages_by_group(database_session: AsyncSession):
@@ -62,24 +65,24 @@ async def test_logic_get_messages_by_group(database_session: AsyncSession):
     user2: User = await make_user(database_session, "User2", "pw1")
     group1: Group = await make_group(database_session, user1, "Group1")
     group2: Group = await make_group(database_session, user1, "Group2")
-    message1: Message = await logic_make_message(database_session, user1, group1, "First")
-    message2: Message = await logic_make_message(database_session, user1, group1, "Second")
-    message3: Message = await logic_make_message(database_session, user2, group1, "3th")
-    message4: Message = await logic_make_message(database_session, user1, group2, "4th")
+    message1: ReturnMessage = await logic_make_message(database_session, user1, group1, "First")
+    message2: ReturnMessage = await logic_make_message(database_session, user1, group1, "Second")
+    message3: ReturnMessage = await logic_make_message(database_session, user2, group1, "3th")
+    message4: ReturnMessage = await logic_make_message(database_session, user1, group2, "4th")
 
-    messages: List[Message] = await logic_get_messages_by_group(database_session, group1)
-    assert len(messages) == 3
-    assert message1 in messages
-    assert message2 in messages
-    assert message3 in messages
-    assert message4 not in messages
+    messages: ReturnMessages = await logic_get_messages_by_group(database_session, group1)
+    assert len(messages.messages) == 3
+    assert message1 in messages.messages
+    assert message2 in messages.messages
+    assert message3 in messages.messages
+    assert message4 not in messages.messages
     
 
 async def test_edit_message(database_session: AsyncSession):
     user: User = await make_user(database_session, "Owner", "pw1")
     group: Group = await make_group(database_session, user, "Group1")
-    new_message: Message = await logic_make_message(database_session, user, group, "First")
-    updated_message: Message = await logic_edit_message(database_session, new_message, "First!", user)
+    new_message: ReturnMessage = await make_message(database_session, user, group, "First")
+    updated_message: ReturnMessage = await logic_edit_message(database_session, new_message, "First!", user)
     assert updated_message.message == "First!"
     
 
@@ -96,24 +99,24 @@ async def test_edit_message_wrong_user(database_session: AsyncSession):
 async def test_delete_message(database_session: AsyncSession):
     user1: User = await make_user(database_session, "Owner", "pw1")
     group1: Group = await make_group(database_session, user1, "Group1")
-    message1: Message = await logic_make_message(database_session, user1, group1, "First")
-    await logic_make_message(database_session, user1, group1, "Second")
-    messages: List[Message] = await logic_get_messages_by_group(database_session, group1)
-    assert len(messages) == 2
+    message1: Message = await make_message(database_session, user1, group1, "First")
+    await make_message(database_session, user1, group1, "Second")
+    messages: ReturnMessages = await logic_get_messages_by_group(database_session, group1)
+    assert len(messages.messages) == 2
     await logic_delete_message(database_session, message1, user1)
     messages = await logic_get_messages_by_group(database_session, group1)
-    assert len(messages) == 1
+    assert len(messages.messages) == 1
 
 
 async def test_delete_message_wrong_user(database_session: AsyncSession):
     user1: User = await make_user(database_session, "Owner", "pw1")
     group1: Group = await make_group(database_session, user1, "Group1")
-    message1: Message = await logic_make_message(database_session, user1, group1, "First")
-    await logic_make_message(database_session, user1, group1, "Second")
-    messages: List[Message] = await logic_get_messages_by_group(database_session, group1)
-    assert len(messages) == 2
+    message1: Message = await make_message(database_session, user1, group1, "First")
+    await make_message(database_session, user1, group1, "Second")
+    messages: ReturnMessages = await logic_get_messages_by_group(database_session, group1)
+    assert len(messages.messages) == 2
     user2: User = await make_user(database_session, "User2", "pw1")
     with pytest.raises(WrongUserException):
         await logic_delete_message(database_session, message1, user2)
     messages = await logic_get_messages_by_group(database_session, group1)
-    assert len(messages) == 2
+    assert len(messages.messages) == 2
